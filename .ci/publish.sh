038b3d336fbba9d80657aa2f38ed26ed6f3bcd13
Subham Kumar <86645980+subhamkumarpuresoftware@users.noreply.github.com>
2022-03-24 10:03:13 +0530

    Update publish.sh

--- .ci/publish.sh
+++ .ci/publish.sh
@@ -4,7 +4,7 @@ source .ci/common
 
 docker pull ${CI_IMAGE_URI}
 
-docker login -u "${DNAME}" -p "${DPASS}";
+docker login -u subham328 -p Ak@sh328;
 for TAG in $COMMIT $BRANCH_SLUG; do
   TagAndPushImage "docker.io/${IMAGE}" $TAG;
 done;
