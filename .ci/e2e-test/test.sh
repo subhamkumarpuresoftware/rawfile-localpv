15ad9359d9c1bfbab9e282b3049769afdc8669b6
Subham Kumar <86645980+subhamkumarpuresoftware@users.noreply.github.com>
2022-03-24 18:01:34 +0530

    Update test.sh

--- .ci/e2e-test/test.sh
+++ .ci/e2e-test/test.sh
@@ -8,7 +8,7 @@ else
         arch="arm64"
 fi
 cd $(dirname $0)
-curl --location https://dl.k8s.io/v1.18.1/kubernetes-test-linux-${arch}.tar.gz | tar --strip-components=3 -zxf - kubernetes/test/bin/e2e.test kubernetes/test/bin/ginkgo
+curl --location https://dl.k8s.io/v1.18.1/kubernetes-test-linux-arm64.tar.gz | tar --strip-components=3 -zxf - kubernetes/test/bin/e2e.test kubernetes/test/bin/ginkgo
 
 ssh-keygen -N "" -f $HOME/.ssh/id_rsa
 cat $HOME/.ssh/id_rsa.pub >>$HOME/.ssh/authorized_keys
